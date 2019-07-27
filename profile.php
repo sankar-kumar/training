c19d5fc263e5ccbc5c2227a15b51f0f6884828e2
Sankar Kumar <52846503+sankar-kumar@users.noreply.github.com>
2019-07-26 23:25:04 -0700

    Add files via upload

--- profile.php
+++ profile.php
@@ -13,7 +13,12 @@ if(!isset($_SESSION['login_user'])){
 <body>
 <div id="profile">
 <b>Welcome : <i><?php echo $login_session; ?></i></b>
-<b><a href="logout.php">Log Out</a></b>
+<p><b>ID:</b><?php echo $login_id; ?></p>
+<p><b>Gender:</b><?php echo $login_gender; ?></p>
+<p><b>Phone code:</b><?php echo $login_phonecode; ?></p>
+<p><b>Phone:</b><?php echo $login_phone; ?></p>
+<p><b>Email:</b><?php echo $login_email; ?></p>
+<p><b><a href="logout.php">Log Out</a></b></p>
 </div>
 </body>
 </html>
--- session.php
+++ session.php
@@ -3,10 +3,16 @@
 $conn = mysqli_connect("localhost", "root", "", "user"); 
 session_start();// Starting Session 
 // Storing Session 
-$user_check = $_SESSION['login_user']; 
+$user_check = $_SESSION['login_user'];
 // SQL Query To Fetch Complete Information Of User 
-$query = "SELECT username from register where username = '$user_check'";
+$query = "SELECT * from register where username = '$user_check'";
 $ses_sql = mysqli_query($conn, $query);
 $row = mysqli_fetch_assoc($ses_sql);
 $login_session = $row['username'];
+$login_id = $row['Id'];
+$login_gender = $row['gender'];
+$login_phonecode = $row['phonecode'];
+$login_phone= $row['phone'];
+$login_email = $row['email'];
+
 ?>
